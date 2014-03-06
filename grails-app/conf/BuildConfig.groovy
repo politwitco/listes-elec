d33f7204145c7b5486153347696392f22cba2cea
unknown <gcernier@NCED2230.iis.amadeus.net>
2014-03-06 14:02:13 +0100

    t

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,6 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.enable.native2ascii = false
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
